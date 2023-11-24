5855b2a288c9d3aeda63ca17df4d8e3fceb5bf79
leandromacedo117 <146491504+leandromacedo117@users.noreply.github.com>
2023-11-24 19:31:55 -0300

    Update script.js
    I finished the project

--- to do in just js/script.js
+++ to do in just js/script.js
@@ -1,4 +1,4 @@
-const finishTodo = document.querySelector(".finish-todo")
+const finishTodo = document.querySelector(".test")
 const inputNew = document.querySelector("#new-task")
 const createtask = document.querySelector(".material-symbols-outlined")
 const listDone = document.querySelector(".list-done")
@@ -6,8 +6,11 @@ const listTask =document.querySelector(".tasks-list")
 const h4 = document.querySelector("h4")
 const span = document.querySelector(".span")
 const button = document.querySelector("button")
+const deletebu = document.querySelector(".delete-todo")
 
 let valor = 1
+
+
 const saveTodo = (text) =>{
     const todo = document.createElement("div")
     todo.classList.add("list-done")
@@ -19,26 +22,24 @@ const saveTodo = (text) =>{
     todo.appendChild(createH4)
 
      const createSpan = document.createElement("span")
-     createSpan.style.display = 'flex'
-     createSpan.style.justifyContent = 'space-between'
+    
      todo.appendChild(createSpan)
 
     const createButton = document.createElement("button")
     createButton.classList.add("finish-todo")
-    createButton.innerHTML = '<i class="fa-solid fa-check"></i>'
+    createButton.innerHTML = ' <span class="material-symbols-outlined">done</span>'
     createSpan.appendChild(createButton)
 
-    const editButton = document.querySelector("button")
+    const editButton = document.createElement("button")
     editButton.classList.add("edit-todo")
-    editButton.innerHTML = ' <i class="fa-solid fa-pen"></i>'
-    createSpan.appendChild(editButton)
-
-     const editButton = document.querySelector("button")
-    editButton.classList.add("edit-todo")
-    editButton.innerHTML = ' <i class="fa-solid fa-pen"></i>'
+    editButton.innerHTML = '<span class="material-symbols-outlined">edit</span>'
     createSpan.appendChild(editButton)
  
-   
+    const deleteButton = document.createElement("button")
+    deleteButton.classList.add("delete-todo")
+    deleteButton.innerHTML = ' <span class="material-symbols-outlined" >close </span>'
+    createSpan.appendChild(deleteButton)
+
 }
 
 
@@ -49,17 +50,31 @@ saveTodo()
 
 })
 
-finishTodo.addEventListener("click" ,function(e){
+document.addEventListener("click" ,function(e){
+   
     const targetEl = e.target;
     const parentEl = targetEl.closest("div")
     let contextTitle;
-   
-
-    if(parentEl && parentEl.querySelector("h4")){
+//    finally tasks
+    if (parentEl && targetEl.querySelector("h4"))
+    {
         contextTitle = parentEl.querySelector("h4").innerText
-    }
+    } 
 
     if(targetEl.classList.contains("finish-todo")){
         parentEl.classList.toggle("finally")
+        console.log('tes')
+    }
+    
+    // remove task
+    if(targetEl.classList.contains("delete-todo")){
+        parentEl.remove()
+    }
+
+    // edit task 
+    if(targetEl.classList.contains("edit-todo")){
+    contextTitle = parentEl.querySelector("h4").innerText
+    inputNew.value = contextTitle
+    parentEl.remove()
     }
 })
